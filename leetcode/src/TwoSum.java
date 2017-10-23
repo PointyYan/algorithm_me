5c0c7b2b3bef6c41627be098433c1d3294e7d827
Yan <yjq97@outlook.com>
2017-10-23 16:18:40 +0800

    add TwoSum

---
+++ leetcode/src/TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] a = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i =0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                a[0]=map.get(target-nums[i]);
+                a[1]=i;
+            }
+            map.put(nums[i],i);
+        }
+        return a;
+    }
+
+}
